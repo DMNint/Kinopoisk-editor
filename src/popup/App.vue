3205c232938f508352827bb3df3b20a6bc3c0909
Unknown <dimosik1232001@gmail.com>
2020-01-15 20:39:41 +0700

    Project inited

---
+++ src/popup/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <transition name="fade">
+      <Main/>
+    </transition>
+  </div>
+</template>
+
+<script>
+import Main from './Main.vue';
+
+export default {
+  data () {
+    return {}
+  },  
+  components: {
+    Main,
+  }
+}
+</script>
---
+++ src/popup/Main.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="main">
+		<header></header>
+		<div class="sub">
+
+		</div>
+	</div>	
+</template>
+
+<script>
+	export default {
+			
+	}
+</script>
+
+<style lang="scss" scoped>
+	.main {
+		width: 350px;
+		height: 100%;
+	}
+
+	header {
+		height: 100px;
+	}
+
+	.sub {
+		height: 300px;
+	}
+</style>
